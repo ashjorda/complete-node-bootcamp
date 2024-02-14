29fba8f09bf6fd1a9d758fc38ed5c3a64390baa6
Ashton Jordan <ashjorda@cisco.com>
2024-02-13 21:19:33 -0600

    Added an asynchronous function for reading files.

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,4 +1,14 @@
+const { error } = require('console');
 const fs = require('fs');
 
-const hello = 'Hello, World!';
-console.log(hello);
+// Blocking, synchronous way
+const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+console.log(textIn);
+const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+fs.writeFileSync('./txt/output.txt', textOut);
+console.log('File written!');
+
+// Non-blocking, asynchronous way
+fs.readFile('./txt/start.txt', 'utf-8', (err, data) => {
+    console.log(data);
+});
