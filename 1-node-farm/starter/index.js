a5a748f7d39e05608b2aa6ee26a8deffc17eb99e
Ashton Jordan <ashjorda@cisco.com>
2024-02-14 18:56:45 -0600

    Loaded the data variable at the top of the code. Then call on it when the /api url is loaded

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -32,21 +32,21 @@ const url = require('url');
 
 
 /////////////////////////////////SERVER////////////////////////
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
+
 const server = http.createServer((req, res) => {
     const pathName = req.url;
 
     if (pathName === '/' || pathName === '/overview') {
-        res.end('This is the OVERVIEW');
+       res.end('This is the OVERVIEW');
     } else if (pathName === '/product') {
-        res.end('This is the PRODUCT');
+       res.end('This is the PRODUCT');
     } else if (pathName === '/api') {
-
-      fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (err, data) => {
-        const productData = JSON.parse(data);
-        res.writeHead(200, {'Content-type': 'application/json'});
-        res.end(data);
-      });
-      
+       res.writeHead(200, {'Content-type': 'application/json'});
+       res.end(data)
     } else {
         res.writeHead(404, {
             'Content-type': 'text/html',
