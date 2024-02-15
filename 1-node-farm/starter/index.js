39244695f4968e01fe715f7cfc8765046aedc3d3
Ashton Jordan <ashjorda@cisco.com>
2024-02-14 18:48:11 -0600

    Created a json response for the API url when loaded.

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -43,10 +43,10 @@ const server = http.createServer((req, res) => {
 
       fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (err, data) => {
         const productData = JSON.parse(data);
-        console.log(productData);
-      })  
-
-        res.end('API');
+        res.writeHead(200, {'Content-type': 'application/json'});
+        res.end(data);
+      });
+      
     } else {
         res.writeHead(404, {
             'Content-type': 'text/html',
