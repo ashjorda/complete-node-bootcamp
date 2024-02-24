8b84c8f50f9980d401b89c3125589468c58274e0
Ashton Jordan <ashton.jordan@gmail.com>
2024-02-24 01:55:12 -0600

    Confirmed array of products to the console. Now I replaed the res.end with the output variable that contains the cardsHtml product data.

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -61,10 +61,9 @@ const server = http.createServer((req, res) => {
     if (pathName === '/' || pathName === '/overview') {
        res.writeHead(200, {'Content-type': 'text/html'});
 
-       const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el));
-       console.log(cardsHtml);
-
-       res.end(tempOverview);
+       const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+       const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+       res.end(output);
 
     // Product page
     } else if (pathName === '/product') {
